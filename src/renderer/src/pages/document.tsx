d13695e893bad66168d35f3a64c55d9a6e54e9c2
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2025-12-12 16:52:12 -0300

    feat(ui): scaffold document page layout with ToC

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,24 @@
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      Exemplo de documento aberto.
-      <Link to="/">Voltar para a tela inicial</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold uppercase text-xs">
+          table of contents
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Link>Front-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>React</ToC.Link>
+            <ToC.Link>Vue</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">Teste</section>
     </main>
   )
 }
